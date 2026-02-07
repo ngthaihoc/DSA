7ba485acc6183b9df45369d593f2fb7c8cb8d7ad
thaiHocng <ngthaihoc.vn@gmail.com>
2026-02-07 21:21:13 +0700

    Longest Substring Without Repeating Characters

--- src/main/java/com/fpt/leetcode/Main.java
+++ src/main/java/com/fpt/leetcode/Main.java
@@ -1,11 +1,26 @@
 package com.fpt.leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-       int n = 7 / 10;
-        System.out.println(Integer.toString(n));
+        String s = "pwwkew";
+        int length = s.length();
+        int l = 0;
+        int r = 0;
+        if (length < 2) return;
+
+        while ( l < length -1 ){
+             r = l + 1 ;
+            if (s.substring(l, l + 1).equals(s.substring(r, r + 1))) {
+                System.out.println(" " + l + " và " + r);
+            }
+            l++;
+        }
+
     }
 }
 
--- src/main/java/com/fpt/leetcode/Solution.java
+++ src/main/java/com/fpt/leetcode/Solution.java
@@ -1,5 +1,8 @@
 package com.fpt.leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length - 1; i++) {
@@ -12,26 +15,47 @@ class Solution {
         return new int[] {};
     }
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//
+//        ListNode dumHead = new ListNode(0);
+//        ListNode curr = dumHead;
+//        int carry = 0;
+//
+//        while (l1 != null || l2 != null || carry > 0){
+//
+//            int x = (l1 != null) ? l1.val : 0;
+//            int y = (l2 != null) ? l2.val : 0;
+//
+//            int sum = carry + x + y;
+//            carry = sum / 10;
+//            curr.next = new ListNode(sum % 10);
+//            curr = curr.next;
+//
+//            if (l1 != null) l1 = l1.next;
+//            if (l2 != null) l2 = l2.next;
+//        }
+//
+//        return dumHead.next;
+//    }
 
-        ListNode dumHead = new ListNode(0);
-        ListNode curr = dumHead;
-        int carry = 0;
 
-        while (l1 != null || l2 != null || carry > 0){
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int maxlong = 0;
+        int l = 0;
 
-            int x = (l1 != null) ? l1.val : 0;
-            int y = (l2 != null) ? l2.val : 0;
-
-            int sum = carry + x + y;
-            carry = sum / 10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
-
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
+        for (int r = 0; r < s.length(); r++) {
+            char c = s.charAt(r);
+            while (set.contains(c)) {
+                set.remove(s.charAt(l));
+                l++;
+            }
+            set.add(c);
+            maxlong = Math.max(maxlong, r - l + 1);
         }
 
-        return dumHead.next;
+        return maxlong;
     }
+
+
 }
