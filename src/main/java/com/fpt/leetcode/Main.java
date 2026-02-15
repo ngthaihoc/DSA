8be82b915851e423a41d019520bce52b4e0ae38c
thaiHocng <ngthaihoc.vn@gmail.com>
2026-02-15 08:21:22 +0700

    String to Integer (atoi) - Runtime 1ms - Memory 43.76

--- src/main/java/com/fpt/leetcode/Main.java
+++ src/main/java/com/fpt/leetcode/Main.java
@@ -5,9 +5,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        int data = 3;
+        String data = "39482948324-344";
 
-        String result = String.valueOf(Solution.reverse( data));
+        String result = String.valueOf(Solution.myAtoi(data));
 
         System.out.println("result : " + result);
 
--- src/main/java/com/fpt/leetcode/Solution.java
+++ src/main/java/com/fpt/leetcode/Solution.java
@@ -154,8 +154,34 @@ class Solution {
     }
 
 
+    public static int myAtoi(String s) {
+        int i = 0;
+        while (i < s.length() && s.charAt(i) == ' ')
+            i++;
 
+        if (i == s.length() ) return 0;
 
+        int sign = 1;
+        if (s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        } else if (s.charAt(i) == '+')
+            i++;
+        int result = 0;
+
+        while (i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+            int digit = s.charAt(i) - '0';
+
+            if (result > (Integer.MAX_VALUE - digit) / 10)
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+
+            result = result * 10 + digit;
+
+            i++;
+        }
+
+        return result * sign;
+    }
 
 }
 
